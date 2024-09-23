999d8db8f4883f99f3ab37ca27bb7dac1d548703
Rohit rajesh Shende <144793226+rohitr99@users.noreply.github.com>
2024-09-23 22:22:44 +0530

    Update Student.js

--- src/Student.js
+++ src/Student.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'// hooks
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 function Student() {
